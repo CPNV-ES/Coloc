032f1c1aa9b58f3d10e8988b938b89291739d1cb
JulienRichoz <julienrichoz@outlook.com>
2019-03-25 17:08:58 +0100

    Add partial model to translate to french

---
+++ Coloc/Models/PartialClasses/Tasks.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Coloc.Models
+{
+    [ModelMetadataType(typeof(TasksMetadata))]
+    public partial class Tasks
+    {
+
+    }
+}
+
+public class TasksMetadata
+{
+    [DisplayName("Titre")]
+    public string Title { get; set; }
+    [DisplayName("Description")]
+    public string Description { get; set; }
+
+}
---
+++ Coloc/Models/PartialClasses/Todos.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Coloc.Models
+{
+    [ModelMetadataType(typeof(TodosMetadata))]
+    public partial class Todos
+    {
+    }
+}
+
+public class TodosMetadata
+{
+    [DisplayName("Titre")]
+    public string Title { get; set; }
+    [DisplayName("Description")]
+    public string Description { get; set; }
+
+}
