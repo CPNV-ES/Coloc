5d6a3657d662d4ef04631466807d3eef81eb2bc3
JulienRichoz <julienrichoz@outlook.com>
2019-03-28 11:21:46 +0100

    Add format date in partial model class

---
+++ Coloc/Models/PartialClasses/UserTasks.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Coloc.Models
+{
+    [ModelMetadataType(typeof(UserTasksMetadata))]
+    public partial class UserTasks
+    {
+    }
+}
+
+public class UserTasksMetadata
+{
+    public int Id { get; set; }
+    public int TaskId { get; set; }
+    public string UserId { get; set; }
+
+    [DisplayName("Débute le ")]
+    [Required(ErrorMessage = "Veuillez entrer une date de début.")]
+    [DataType(DataType.Date)]
+    [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+    public DateTime BeginTask { get; set; }
+
+    [DisplayName("Débute le ")]
+    [Required(ErrorMessage = "Veuillez enter une date de fin")]
+    [DataType(DataType.Date)]
+    [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+    public DateTime EndTask { get; set; }
+    public DateTime? FinishTask { get; set; }
+    public byte State { get; set; }
+}
