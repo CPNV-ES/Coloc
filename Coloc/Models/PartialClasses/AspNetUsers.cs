f76469d596fbeb1a820957b0774927ac28092f02
JulienRichoz <julienrichoz@outlook.com>
2019-03-31 21:18:20 +0200

    Add Users partial model

---
+++ Coloc/Models/PartialClasses/AspNetUsers.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Description : Partial Model to avoid overriding models on Scaffold models
+ * Translate words to french, test validation
+ * 
+ * Author : Julien Richoz / SI-T2a / CPNV-ES
+ * Date : 31.03.2019
+ * */
+
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+namespace Coloc.Models
+{
+    [ModelMetadataType(typeof(AspNetUsers))]
+    public partial class AspNetUsers
+    {
+
+    }
+}
+
+public class AspNetUsersMetadata { 
+        public string Id { get; set; }
+
+        [DisplayName("Nom d'utilisateur")]
+        public string UserName { get; set; }
+}
