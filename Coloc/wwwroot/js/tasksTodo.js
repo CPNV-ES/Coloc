92230dba0a2487b387bc8b109ecb7b416da61321
JulienRichoz <julienrichoz@outlook.com>
2019-03-31 21:21:37 +0200

    Add dynamic submit button verification

--- Coloc/wwwroot/js/tasksTodo.js
+++ Coloc/wwwroot/js/tasksTodo.js
@@ -1,23 +1,68 @@
 ﻿$(document).ready(function () {
     /* Add new task to a user*/
     $("#add-task-todo-button").on("click", function (e) {
-        console.log('AHAHAHA');
         e.preventDefault();
         //get input data
-        console.log($("#task-todo-add #title").val());
         var description = $("#task-todo-add #description").val();
         var title = $("#task-todo-add #title").val();
         var todoId = $("#task-todo-add #todoId").val();
-        //send data to api to store new vaucher
+
+        //send data to api
         add(title, description, todoId);
     });
+
+    // On Add task to a todo : check form validation (/Todos/Details/TodoID)
+    $("#task-todo-add").on("change paste keyup click", function(){
+        // Activate/desactive submit button if form is valid or not
+        if ($(this).valid()) {
+            $("#add-task-todo-button").removeAttr("disabled").removeClass("disabled")
+        }
+        else {
+            $("#add-task-todo-button").attr("disabled")
+            $("#add-task-todo-button").addClass("disabled")
+        }
+    });
+
+    // On create new todo : check form validation (Todos/Create)
+    $("#todo-create-form").on("change paste keyup click", function () {
+        // Activate/desactive submit button if form is valid or not
+        if ($(this).valid()) {
+            $("#todo-create-submit").removeAttr("disabled").removeClass("disabled")
+        }
+        else {
+            $("#todo-create-submit").attr("disabled")
+            $("#todo-create-submit").addClass("disabled")
+        }
+    });
+
+    // On Edit new todo : check form validation (Todos/Edit/TodoId)
+    $("#todo-edit-form").on("change paste keyup click", function () {
+        // Activate/desactive submit button if form is valid or not
+        if ($(this).valid()) {
+            $("#todo-edit-submit").removeAttr("disabled").removeClass("disabled")
+        }
+        else {
+            $("#todo-edit-submit").attr("disabled")
+            $("#todo-edit-submit").addClass("disabled")
+        }
+    });
+
+    // On Edit new todo : check form validation (Tasks/Edit/TaskId)
+    $("#task-edit-form").on("change paste keyup click", function () {
+        // Activate/desactive submit button if form is valid or not
+        if ($(this).valid()) {
+            $("#task-edit-submit").removeAttr("disabled").removeClass("disabled")
+        }
+        else {
+            $("#task-edit-submit").attr("disabled")
+            $("#task-edit-submit").addClass("disabled")
+        }
+    });
+
 });
 
-function add(title, description, todoId){
-    let current_datetime = new Date()
-    let formatted_date = current_datetime.getFullYear() + "/" + (current_datetime.getMonth() + 1) + "-" + current_datetime.getDate() + " " + current_datetime.getHours() + ":" + current_datetime.getMinutes() + ":" + current_datetime.getSeconds()
-
-    // use tha api to store a new purchase for the current customer and return a partial view without reload the page
+function add(title, description, todoId){ 
+    // use the api to store a new todo for the current customer and return a partial view without reload the page
     $.ajax({
         accepts: "application/json",
         contentType: "application/json",
@@ -36,10 +81,6 @@ function add(title, description, todoId){
         })
         .fail(function () {
   
-            alert("La tâche n'a pas été créée");
+            alert("La tâche n'a pas été créée. Veuillez contacter le support.");
         });
 };
-
-function update() {
-
-}
